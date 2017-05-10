8da84f83c872ad9a03c31f32fdc7dc2da1f1d819
liuzechuan <13691338376@163.com>
2017-05-10 14:02:19 +0800

    jishiyu-commit

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -9,10 +9,10 @@
 | It's a breeze. Simply tell Laravel the URIs it should respond to
 | and give it the controller to call when that URI is requested.
 |
-*/
+ */
 
 Route::get('/', function () {
-    return view('welcome');
+	return view('welcome');
 });
 
 /*
@@ -24,26 +24,24 @@ Route::get('/', function () {
 | it contains. The "web" middleware group is defined in your HTTP
 | kernel and includes session state, CSRF protection, and more.
 |
-*/
+ */
 
-Route::group(['namespace'=>'Admin','middleware' => ['web']], function () {
-    Route::controller('admin/index','IndexController');
-    Route::controller('admin/login','LoginController');
-    Route::controller('admin/user','UserController');
-    Route::controller('admin/channel','ChannelController');
-    Route::controller('admin/business','BusinessController');
-    Route::controller('admin/card','CardController');
-    Route::controller('admin/slide','SlideController');
-    Route::controller('admin/slidecat','SlideCatController');
-    Route::controller('admin/role','RoleController');
-    Route::controller('admin/adminuser','AdminUserController');
-    Route::controller('admin/applog','AppLogController');
+Route::group(['namespace' => 'Admin', 'middleware' => ['web']], function () {
+	Route::controller('admin/index', 'IndexController');
+	Route::controller('admin/login', 'LoginController');
+	Route::controller('admin/user', 'UserController');
+	Route::controller('admin/channel', 'ChannelController');
+	Route::controller('admin/business', 'BusinessController');
+	Route::controller('admin/card', 'CardController');
+	Route::controller('admin/slide', 'SlideController');
+	Route::controller('admin/slidecat', 'SlideCatController');
+	Route::controller('admin/role', 'RoleController');
+	Route::controller('admin/adminuser', 'AdminUserController');
+	Route::controller('admin/applog', 'AppLogController');
 });
 
-Route::group(['namespace'=>'Api','middleware' => ['web']], function () {
-    Route::controller('api/alipay','AlipayController');
-    Route::controller('api/cashbus','CashbusController');
+Route::group(['namespace' => 'Api', 'middleware' => ['web']], function () {
+	Route::controller('api/alipay', 'AlipayController');
+	Route::controller('api/cashbus', 'CashbusController');
+
 });
-
-
-
