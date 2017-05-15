dd672d31d6cb3c7461f697ead6ffd18e92d80d10
liuzechuan <13691338376@163.com>
2017-05-15 17:40:31 +0800

    芝麻加密

--- app/Http/Controllers/Api/AlipayController.php
+++ app/Http/Controllers/Api/AlipayController.php
@@ -8,6 +8,8 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
+require  '/zmxy-sdk/zmop/RSAUtil.php';
+
 class AlipayController extends ApiController
 {
     /**
@@ -16,5 +18,21 @@ class AlipayController extends ApiController
      */
     public function anyAuthorize(Request $request){
         //dd($request->all());
+    }
+
+    public function postEncrypt(Request $request){
+        $identity_type=$request->identity_type;
+        if(!$identity_type){
+            return $this->msg('0001','参数不正确');
+        }
+        $identity_param=$request->identity_param;
+        if(!$identity_param){
+            return $this->msg('0001','参数不正确');
+        }
+        $str="identity_type=".urlencode($identity_type)."&identity_param=".urlencode($identity_param);
+
+        $params=urlencode(base64_encode(\RSAUtil::rsaEncrypt($str,"d:\Projects\jishiyu\public\zmxy-sdk\public_key.pem")));
+        return $this->msg('0000','成功',['params'=>$params]);
+
     }
 }
